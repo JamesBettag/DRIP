9562175810a3a38bf7a7905c3557c85577c8e9ab
bettagj <bettagj@spu.edu>
2020-04-18 18:35:32 -0700

    tried to get mysql to insert. didn't work

--- Website/db.js
+++ Website/db.js
@@ -1,12 +1,27 @@
+/*
+const mysqlx = require('@mysql/xdevapi')
+
+mysqlx
+    .getSession('mysqlx://bettagj:Password0*@leia.cs.spu.edu:33060/gms')
+    .then(session=> {
+        console.log(session.inspect())
+    })
+*/
+
 var mysql = require('mysql')
 var pool
 
 exports.connect = function ConnectionHandler(done){
     pool = mysql.createPool({
+        connectionLimit : 1000,
+        connectTimeout  : 60 * 60 * 1000,
+        acquireTimeout  : 60 * 60 * 1000,
+        timeout         : 60 * 60 * 1000,
         host: "leia.cs.spu.edu",
         user: "bettagj",
         password: "Password0*",
-        database: "gms"
+        database: "gms",
+        insecureAuth: true
     });
     done();
 }
--- Website/models/model.js
+++ Website/models/model.js
@@ -1,1 +1,12 @@
 var db = require('../db')
+
+exports.insertNewUser = function InserNewUserHandler(fname, lname, email, pass, done) {
+    db.get().query(
+        'INSERT INTO account (first_name, last_name, email, password) VALUES (?, ?, ?, ?)', [fname, lname, email, pass], function InserNewUserQueryHandler(err, result) {
+            if(err) {
+                return done(err)
+            }
+            done(null, result.insertId)
+        }
+    )
+}
