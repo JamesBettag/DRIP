917520ed4a2585f467262c1a18d5d371cbd943e4
Tad Okazaki <okazakit@spu.edu>
2020-04-24 12:43:48 -1000

    Partially active email validation functionality

--- Website/verification-email.js
+++ Website/verification-email.js
@@ -14,8 +14,8 @@ exports.sendVerificationEmail = function sendVerificationEmailFunction(email, ac
     from: 'notifications.drip@gmail.com',
     subject: 'DRIP Account Verification',
     text: '?',    //This field does not seem to appear on emails
-    html: '<strong>These are some important words</strong><br><p>Validate your email here:<br>http://localhost:3000/verification?hash=' + accHash +'</p>'
-
+    html: "<strong>Account has been created!</strong><br><p>Validate your email here:<br><a href='http://localhost:3000/verification?hash=" + accHash +"'>Validate</a></p>"
+                                                            // "<a " + target + "href='YourURLHere?dbid=db&id=" + openLink +"'>HyperLinkText</a>";
 
     ,     //End html words in text box
   };
