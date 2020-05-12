3fcc4fc99db03bda675ec2d9dff23c9253ba9a9f
bettagj <bettagj@spu.edu>
2020-05-12 16:29:36 -0700

    added comments

--- Website/routers/piRouter.js
+++ Website/routers/piRouter.js
@@ -7,11 +7,15 @@ router.use(methodOverride('_method'))
 
 router.get('/insert', async (req, res) => {
     const { mac, data } = req.query
+    // get plant ID from db
     plantId = await dataModel.getPlantID(mac)
     if(plantId != null) {
+        // check if plant id exists, if so, insert the data to the plant
         await dataModel.insertMoistureData(plantId, data)
+        // send code to pi for it to interpret success
         res.send("0")
     } else {
+        // no plant was found, check if device exists
         const deviceId = await dataModel.getDeviceID(mac)
         if(deviceId != null) {
             // code to send to pi that user doesn't have plant registered to a sensor
