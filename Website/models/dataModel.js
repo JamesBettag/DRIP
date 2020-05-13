fb8a827adafa176b21b2fdcebacc3a40112e76bd
bettagj <bettagj@spu.edu>
2020-05-12 20:42:28 -0700

    sql error

--- Website/models/dataModel.js
+++ Website/models/dataModel.js
@@ -3,7 +3,7 @@ const db = require('../config/db')
 exports.getGraphData = function(email, startDate, stopDate) {
     return new Promise(function(resolve, reject) {
         db.get().query(
-            "SELECT plant.minimum, plant.maximum, data.moisture, data.time plant.name" +
+            "SELECT plant.minimum, plant.maximum, data.moisture, data.time, plant.name" +
             "FROM data, plant, device, account " + 
             "WHERE (data.plant_id = plant.plant_id) AND (plant.account_id = account.account_id) AND (account.email = ?) AND (data.time) BETWEEN ? AND ?", [email, startDate, stopDate], (err, result, fields) => {
                 if(err) {
